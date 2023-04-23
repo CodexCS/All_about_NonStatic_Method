c138b19514a0092d59d62b30b66f11287ad38f0b
Arnav Bhagwat <76836227+Thindino@users.noreply.github.com>
2023-04-24 00:18:18 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,8 @@
+
+public class Car {
+	String color;
+	void Status() {
+		System.out.println("90% fuel is used");
+	}
+
+}
---
+++ Car_Runner.java
@@ -0,0 +1,12 @@
+
+public class Car_Runner {
+
+	public static void main(String[] args) {
+		Car Mercedes = new Car();
+		Car Audi = new Car();
+		Mercedes.Status();
+		Mercedes.color = "Red";
+		Audi.color = "blue";
+	}
+
+}
